9a600f91fc561bfc587ee88174cce3f7e95f5e1a
Ares0098 <muhammadrezkyhamesi@gmail.com>
2022-08-26 08:18:22 +0700

    Credit Risk Prediction with Deployment

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ import numpy as np
 import pickle
 
 app = Flask(__name__) 
-model = pickle.load(open('models/modelLR.pkl', 'rb'))
-model1 = pickle.load(open('models/modelSvc.pkl', 'rb'))
-sc = pickle.load(open('models/scaler.pkl', 'rb'))
-
+model = pickle.load(open('models/Final_Model.pkl', 'rb'))
 
 @app.route("/")
 def home():
@@ -15,56 +12,39 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
 
-    mintemp         = float(request.form['mintemp'])
-    rainfall        = float(request.form['rainfall'])
-    windgustdir     = float(request.form['windgustdir'])
-    windgustspeed   = float(request.form['windgustspeed'])
-    windir9         = float(request.form['windir9'])
-    windir3         = float(request.form['windir3'])
-    windspeed9      = float(request.form['windspeed9'])
-    windspeed3      = float(request.form['windspeed3'])
-    humidity9       = float(request.form['humidity9'])
-    humidity3       = float(request.form['humidity3'])
-    raintoday       = float(request.form['raintoday'])
-    location        = float(request.form['location'])
-    metode          = float(request.form['metode'])
+    val = []
 
+    int_rate        = float(request.form['int_rate'])
+    val = np.append(val, int_rate)
 
-    val = [mintemp, rainfall, windgustdir, windgustspeed, windir9, windir3, windspeed9, windspeed3, humidity9, humidity3]
-    val = sc.transform([val])
-    val = val.reshape(10,)
+    grade           = float(request.form['grade'])
+    val = np.append(val, grade)
 
+    sub_grade       = float(request.form['sub_grade'])
+    val = np.append(val, sub_grade)
 
-    if raintoday == 1:
-        val = np.append(val, 1)
-    elif raintoday == 0:
-        val = np.append(val, 0)
-    else:
-        print('ERROR!')
+    total_rec_prncp = float(request.form['total_rec_prncp'])
+    val = np.append(val, total_rec_prncp)
 
-    locations = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5,
-                6:6, 7:7, 8:8, 9:9, 10:10, 11:11,
-                12:12, 13:13, 14:14, 15:15, 16:16, 17:17,
-                18:18, 19:19, 20:20, 21:21, 22:22, 23:23,
-                24:24, 25:25, 26:26, 27:27, 28:28, 29:29,
-                30:30, 31:31, 32:32, 33:33, 34:34, 35:35,
-                36:36, 37:37, 38:38, 39:39, 40:40, 41:41, 
-                42:42, 43:43, 44:44, 45:45, 46:46}
+    total_pymnt_inv = float(request.form['total_pymnt_inv'])
+    val = np.append(val, total_pymnt_inv)
 
-    for i in range(0,47):
-        if locations[location]==i:
-            val = np.append(val, 1)
-        else:
-            val = np.append(val, 0)
+    total_pymnt     = float(request.form['total_pymnt'])
+    val = np.append(val, total_pymnt)
 
-    if metode == 1:
-        val_predict = model.predict([val])
-        return render_template('predict.html', prediction=val_predict)
-    elif metode == 2:
-        val_predict = model1.predict([val])
-        return render_template('predict.html', prediction=val_predict)
-    else:
-        print('ERROR!')
+    out_prncp       = float(request.form['out_prncp'])
+    val = np.append(val, out_prncp)
+
+    out_prncp_inv   = float(request.form['out_prncp_inv'])
+    val = np.append(val, out_prncp_inv)
+
+    last_pymnt_amnt = float(request.form['last_pymnt_amnt'])
+    val = np.append(val, last_pymnt_amnt)
+
+    val = val.reshape(9,)
+
+    val_predict = model.predict([val])
+    return render_template('predict.html', prediction=val_predict)
 
 if __name__ == "__main__":
     app.run(debug=True)
